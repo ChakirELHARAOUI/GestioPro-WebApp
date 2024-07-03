cedac163a5ed2d30b4d3acdc2a5540fd77191c3b
ChakirELHARAOUI <164023913+ChakirELHARAOUI@users.noreply.github.com>
2024-07-03 14:43:59 +0200

    Example of sensible file

---
+++ backend/config/config.example.js
@@ -0,0 +1,11 @@
+const config = {
+    development: {
+      username: 'your_username',
+      password: 'your_password',
+      database: 'your_database',
+      host: 'your_host',
+      dialect: 'your_dialect' //postgres if you use postgresql
+    }
+}
+
+module.exports = config;
